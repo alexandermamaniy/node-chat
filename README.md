6096418c629e810b18ef99c241c7729d94e63443
Alex <akeymy4@gmail.com>
2018-10-28 16:44:30 -0400

    ready for production

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Este es un pequeño servidor de express listo para ejecutarse y servir la carpeta public en la web.
 
-Recuerden que deben de reconstruir los módulos de node con el comando
 
 ```
 npm install
+npm start
 ```
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -18,21 +18,24 @@ io.on('connection', (client) => {
     client.join(data.sala)
 
     usuarios.agregarPersona(client.id, data.nombre, data.sala)
-    client.broadcast.to(data.sala).emit('listarPersona', usuarios.getPersonasPorSala(data.sala))
+    client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala))
+
+    client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${data.nombre} se unio`))
     callback(usuarios.getPersonasPorSala(data.sala))
   })
 
   client.on('disconnect', () => {
     let personaBorrada = usuarios.borrarPersona(client.id)
     client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${personaBorrada.nombre} salio!!`))
-    client.broadcast.to(personaBorrada.sala).emit('listarPersona', usuarios.getPersonasPorSala(personaBorrada.sala))
+    client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala))
   })
 
   // enviar mensaje a todo el mundo
-  client.on('crearMensaje', (data) => {
+  client.on('crearMensaje', (data, callback) => {
     let persona = usuarios.getPersona(client.id)
     let mensaje = crearMensaje(persona.nombre, data.mensaje)
     client.broadcast.to(persona.sala).emit('crearMensaje', mensaje)
+    callback(mensaje)
   })
 
   // Mensajes privados de los clientes
